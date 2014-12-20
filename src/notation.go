57b49598d0ebc3e7d61f941d5472f1d54f865b86
Andrew Backes <rbackes@ucla.edu>
2014-12-19 21:27:01 -0800

    pawn capture promotion notation bug fixed. added notation unit tests.

--- src/notation.go
+++ src/notation.go
@@ -48,51 +48,6 @@ Nge2
    +---+---+---+---+---+---+---+---+
      a   b   c   d   e   f   g   h
 
-
-TODO: is this still a bug?
-
-BUG:
-
-> ucinewgame
-> position startpos moves d2d4 g8f6 c2c4 e7e6 b1c3 f8b4 g1f3 c7c5 d4c5 b8c6 c1d2 b4c5 e2e3 d7d5 f1e2 e8g8 e1g1 d5c4 e2c4 e6e5 e3e4 c8g4 a1c1 c6d4 c4e2 d4f3 e2f3 g4f3 g2f3 c5d4 d1e2 d8b6 d2e3 a8c8 f1d1 f8d8 g1g2 a7a6 d1d3 f6d7 c3d5 b6g6 g2h1 c8c1 e3c1 g6e6 c1g5 f7f6 g5e3 d7b6 e3d4 e5d4 e2d1 b6d5 d3d4 g8f7 d4d5 d8d5 e4d5 e6d6 h1g2 f6f5 d1d3 f7g6 h2h4 g6f6 d3d4 f6g6 f3f4 d6e7 d5d6 e7d7 g2g3 h7h6 d4c5 d7e6 c5e5 e6f7 b2b3 g6h7 g3f3 h7g6 f3e3 b7b5 e3d4 f7a7 e5c5 a7b7 h4h5 g6f6 c5e5 f6f7 e5f5 f7e8 f5e6 e8f8 d6d7 b7b8 d4c5 b8c7 c5d5 c7d8 d5c6 d8a8 c6b6 a8d8 b6a6 d8b8 e6e5 b8d8 e5d6 f8f7 a6b5 f7g8 f4f5 g8h8 a2a4 h8g8 b3b4 g7g6 f5g6 g8g7 d6d4 g7f8 b5c6 f8g8 d4d5 g8h8 d5e5 h8g8 e5e8 d8e8 d7e8Q g8g7 e8f7 g7h8 f7f8
-Error: index out of range.
-5Q1k/8/2K3Pp/7P/PP6/8/5P2/8 b - - 2 69
-0000
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |[Q]|   | k |      WHITE 00:00.285   [BLACK 00:00.970]
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |[ ]|   |   |
-+---+---+---+---+---+---+---+---+
-|   |   | K |   |   |   | P | p |      Enpassant: None
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |   |   | P |      Castling Rights: ----
-+---+---+---+---+---+---+---+---+
-| P | P |   |   |   |   |   |   |
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |   |   |   |
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   | P |   |   |
-+---+---+---+---+---+---+---+---+
-|   |   |   |   |   |   |   |   |
-+---+---+---+---+---+---+---+---+
-panic: runtime error: index out of range
-
-goroutine 21 [running]:
-runtime.panic(0x15ace0, 0x2551fc)
-	/usr/local/go/src/pkg/runtime/panic.c:279 +0xf5
-main.InternalizeNotation(0x2082fc1c0, 0x208462bc9, 0x4, 0x0, 0x0)
-	/Users/Andrew/Projects/Tourney/src/tourney/notation.go:72 +0xb2f
-main.ExecuteNextTurn(0x2082fc1c0, 0x0)
-	/Users/Andrew/Projects/Tourney/src/tourney/game.go:147 +0x561
-main.PlayGame(0x2082fc1c0, 0x0, 0x0)
-	/Users/Andrew/Projects/Tourney/src/tourney/game.go:100 +0x12d
-main.RunTourney(0x2082c8dd0, 0x0, 0x0)
-	/Users/Andrew/Projects/Tourney/src/tourney/tourney.go:131 +0xfc5
-main.func·004()
-	/Users/Andrew/Projects/Tourney/src/tourney/command.go:42 +0x32
-created by main.doCommand
-	/Users/Andrew/Projects/Tourney/src/tourney/command.go:46 +0x689
-
 */
 
 package main
@@ -149,42 +104,77 @@ func InternalizeNotation(G *Game, moveToParse string) (string, error) {
 		return parsed, nil
 	}
 
+	/* OLD CODE */
+
 	// First check for an ambiguous promotion:
 	//SAN := "([a-h]?)([0-9]?)([a-h][0-9])([=])([BNQR])([+#]?)"
 	//p, _ := regexp.Compile(SAN)
 
 	// Breakdown the SAN:
-	SAN := "([BKNPQR]?)([a-h]?)([0-9]?)([x=]?)([BKNPQR]|[a-h][1-8])([+#]?)"
+	// SAN := "([BKNPQR]?)([a-h]?)([0-9]?)([x=]?)([BKNPQR]|[a-h][1-8])([=]?[BKNPQR]?)([+#]?)"
+
+	/*
+		SAN := "([BKNPQR]?)([a-h]?)([0-9]?)([x=]?)([BKNPQR]|[a-h][1-8])([+#]?)"
+
+		//		  (piece)    (from)  (from)  (cap) (dest)      (promotion)        (chk  )
+		//SAN := "([BKNPQR]?)([a-h]?)([0-9]?)([x]?)([a-h][1-8])([=]?[BNPQRbnpqr]?)([+#]?)"
+		r, _ := regexp.Compile(SAN)
+
+		matched := r.FindStringSubmatch(moveToParse)
+		if len(matched) == 0 {
+			//fmt.Println("Error: index out of range.")
+			//fmt.Println(G.FEN())
+			//fmt.Println(moveToParse)
+			//G.PrintHUD()
+			return moveToParse, errors.New("Error parsing move from engine: '" + moveToParse + "'")
+		}
+		// For the sake of sanity, lets name some stuff:
+		piece := matched[1]
+		fromFile := matched[2]
+		fromRank := matched[3]
+		action := matched[4]      // capture or promote
+		destination := matched[5] //or promotion piece if action="="
+		//check := matched[6]       //or mate
+		var promote string
+
+		if piece == "" {
+			piece = "P"
+		}
+
+		// TODO: what about promotion captures? or ambiguous promotions?
+		if action == "=" {
+			promote = destination
+			destination = fromFile + fromRank
+			fromFile = ""
+			fromRank = ""
+		}
+	*/
+	/************/
+
+	/* NEW CODE */
+
+	//	    (piece)    (from)  (from)  (cap) (dest)      (promotion)        (chk  )
+	SAN := "([BKNPQR]?)([a-h]?)([0-9]?)([x]?)([a-h][1-8])([=]?[BNPQRbnpqr]?)([+#]?)"
 	r, _ := regexp.Compile(SAN)
 
 	matched := r.FindStringSubmatch(moveToParse)
 	if len(matched) == 0 {
-		//fmt.Println("Error: index out of range.")
-		//fmt.Println(G.FEN())
-		//fmt.Println(moveToParse)
-		//G.PrintHUD()
 		return moveToParse, errors.New("Error parsing move from engine: '" + moveToParse + "'")
 	}
-	// For the sake of sanity, lets name some stuff:
+
 	piece := matched[1]
 	fromFile := matched[2]
 	fromRank := matched[3]
-	action := matched[4]      // capture or promote
+	//action := matched[4]      // capture or promote
 	destination := matched[5] //or promotion piece if action="="
 	//check := matched[6]       //or mate
-	var promote string
+	promote := strings.Replace(matched[6], "=", "", 1)
 
 	if piece == "" {
 		piece = "P"
 	}
 
-	// TODO: what about promotion captures? or ambiguous promotions?
-	if action == "=" {
-		promote = destination
-		destination = fromFile + fromRank
-		fromFile = ""
-		fromRank = ""
-	}
+	/*************/
 
 	origin, err := originOfPiece(piece, destination, fromFile, fromRank, G)
 	if err != nil {
@@ -192,7 +182,7 @@ func InternalizeNotation(G *Game, moveToParse string) (string, error) {
 		fmt.Println(G.FEN())
 		fmt.Println(moveToParse)
 		G.PrintHUD()
-		return moveToParse, errors.New("Error finding source square of move: '" + moveToParse + "'")
+		return moveToParse, errors.New("Error finding source square of move: '" + moveToParse + "'.")
 	}
 
 	// Some engines dont tell you to promote to queen, so assume so in that case:
@@ -254,6 +244,14 @@ func originOfPiece(piece, destination, fromFile, fromRank string, G *Game) (stri
 		}
 
 	}
+	//DEBUG:
+	/*
+		fmt.Println("params: ", piece, destination, fromFile, fromRank)
+		fmt.Println("color: ", color)
+		fmt.Println("legalMoves:", legalMoves)
+		fmt.Println("eligableMoves:", eligableMoves)
+		fmt.Println("eligableSquares:", eligableSquares)
+	*/
 	return "", errors.New("Notation: Can not find source square.")
 }
 
---
+++ src/notation_test.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestInternalizeNotation(t *testing.T) {
+
+	tests := [][]string{
+		{
+			// 0
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"e2e4",
+			"e2e4",
+		},
+		{
+			// 1
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"e4",
+			"e2e4",
+		},
+		{
+			// 2
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"b1c3",
+			"b1c3",
+		},
+		{
+			// 3
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"Nc3",
+			"b1c3",
+		},
+		{
+			// 4
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"c3",
+			"c2c3",
+		},
+		{
+			// 5
+			"2r5/4R2P/p7/8/3k4/1P4P1/P4PK1/8 w - - 0 47",
+			"h8Q",
+			"h7h8q",
+		},
+		{
+			// 6
+			"2r5/4R2P/p7/8/3k4/1P4P1/P4PK1/8 w - - 0 47",
+			"h7h8q",
+			"h7h8q",
+		},
+		{
+			// 7
+			"r1bqk2r/pppn1pbp/3p2p1/5p2/2PP4/2N1P1P1/PP3PBP/R2QK1NR w KQkq - 1 8", // FEN
+			"Nge2", // SAN
+			"g1e2", // PCN
+		},
+		{
+			// 8
+			"1q4k1/P5p1/1K6/2pb4/7B/8/8/8 w - - 0 145",
+			"axb8=Q+",
+			"a7b8q",
+		},
+		{
+			// 9
+			"3r3k/4P3/7K/8/8/3p2P1/7P/8 w - - 0 69",
+			"exd8=Q#",
+			"e7d8q",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"Qh3",
+			"f3h3",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"Nb5",
+			"c3b5",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"Pxh3",
+			"g2h3",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"xh3",
+			"g2h3",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"O-O-O",
+			"e1c1",
+		},
+		{
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+			"O-O",
+			"e1g1",
+		},
+		{
+			"8/2p5/3p4/KP5k/1R3p1r/4P1P1/8/8 w - - 0 1",
+			"gf4",
+			"g3f4",
+		},
+		{
+			"8/2p5/3p4/KP5k/1R3p1r/4P1P1/8/8 w - - 0 1",
+			"ef4",
+			"e3f4",
+		},
+		{
+			"8/2p5/3p4/KP5k/1R3p1r/4P1P1/8/8 w - - 0 1",
+			"gxf4",
+			"g3f4",
+		},
+		{
+			// 19
+			"8/2p5/3p4/KP5k/1R3p1r/4P1P1/8/8 w - - 0 1",
+			"Rf4",
+			"b4f4",
+		},
+		{
+			// 20
+			"8/2p5/3p4/KP5k/1R3p1r/4P1P1/8/8 w - - 0 1",
+			"Rxf4",
+			"b4f4",
+		},
+	}
+
+	for i, _ := range tests {
+		tester := NewGame()
+		tester.LoadFEN(tests[i][0])
+		SAN := tests[i][1]
+		PCN := tests[i][2]
+		answer, err := InternalizeNotation(&tester, SAN)
+		if answer != PCN {
+			t.Error(i, "FAILED: Expected", PCN, "got", answer, "-", err)
+		} else {
+			t.Log(i, "PASSED. ")
+		}
+
+	}
+}
