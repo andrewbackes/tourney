4bcd7f3879c8220c92a0c44558b33ab04fc771f7
Andrew Backes <rbackes@ucla.edu>
2017-03-21 17:39:34 -0700

    Basic test to check coverage

--- api/controller_test.go
+++ api/controller_test.go
@@ -1,10 +1,21 @@
 package api
 
-
 import (
 	"testing"
+	"bytes"
 )
 
-func TestPlaceholder(t *testing.T) {
-
+func TestWriteJSON(t *testing.T) {
+	var b bytes.Buffer
+	m  := make(map[string]string)
+	m["mykey"] = "myval"
+	writeJSON(m, &b)
+	expected := `{
+  "mykey": "myval"
+}
+`
+	actual := b.String() 
+	if actual !=  expected {
+		t.Error("Got:", actual, "\nWanted:", expected)
+	}
 }
