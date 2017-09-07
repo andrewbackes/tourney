a4d97c1f8f4718999e03663a193fcfa6b272ae89
andrewbackes <rbackes@ucla.edu>
2017-09-06 18:53:57 -0700

    Openings applied to tournament on generation

--- data/services/tournament/openings.go
+++ data/services/tournament/openings.go
@@ -1,7 +1,6 @@
 package tournament
 
 import (
-	"fmt"
 	"github.com/andrewbackes/chess/book"
 	"github.com/andrewbackes/chess/fen"
 	"github.com/andrewbackes/chess/game"
@@ -13,6 +12,7 @@ import (
 )
 
 // setGameOpenings will set the state of all games in the tournament. It chooses the state based on the opening book selected.
+// Assumes that games come in matchup pairs.
 // TODO(andrewbackes): increment
 // TODO(andrewbackes): panic handling
 func setGameOpenings(t *models.Tournament) {
@@ -22,11 +22,22 @@ func setGameOpenings(t *models.Tournament) {
 	if t.Settings.Opening.Depth > t.Settings.Opening.Book.MaxDepth {
 		panic("book depth is less than what is specified in the tournament")
 	}
-	b := openBook("/Users/Andrew/tourney_books/2700draw.bin")
+	b := openBook(t.Settings.Opening.Book.FilePath)
 	o := openingPos(t.Settings.TimeControl)
 	pl := []*models.Position{posToPos(o)}
-	deepen(t.Settings.Opening.Depth, o, pl, b, func(pl []*models.Position) {
-		fmt.Println(pl)
+	index := 0
+	complete := false
+	deepen(t.Settings.Opening.Depth, o, pl, b, complete, func(l []*models.Position) {
+		if !complete {
+			for j := 0; j < 2; j++ {
+				t.Games[index].Positions = posPtrsToVals(l)
+				index++
+				if index >= len(t.Games)-1 {
+					complete = true
+					break
+				}
+			}
+		}
 	})
 }
 
@@ -58,7 +69,7 @@ func openingPos(tc game.TimeControl) *position.Position {
 	return o
 }
 
-func deepen(d int, orig *position.Position, pl []*models.Position, b *book.Book, callback func([]*models.Position)) {
+func deepen(d int, orig *position.Position, pl []*models.Position, b *book.Book, complete bool, callback func([]*models.Position)) {
 	if d == 0 {
 		callback(pl)
 		return
@@ -69,7 +80,10 @@ func deepen(d int, orig *position.Position, pl []*models.Position, b *book.Book,
 			nextSlice := make([]*models.Position, len(pl))
 			copy(nextSlice, pl)
 			nextSlice = append(nextSlice, posToPos(next))
-			deepen(d-1, next, nextSlice, b, callback)
+			deepen(d-1, next, nextSlice, b, complete, callback)
+			if complete {
+				return
+			}
 		}
 	}
 }
@@ -91,4 +105,23 @@ func posToPos(orig *position.Position) *models.Position {
 		},
 		LastMove: orig.LastMove,
 	}
+}
+
+func posPtrsToVals(ptrs []*models.Position) []models.Position {
+	n := make([]models.Position, len(ptrs))
+	for i, p := range ptrs {
+		n[i] = models.Position{
+			FEN:      p.FEN,
+			LastMove: p.LastMove,
+			MovesLeft: map[piece.Color]int{
+				piece.White: p.MovesLeft[piece.White],
+				piece.Black: p.MovesLeft[piece.Black],
+			},
+			Clocks: map[piece.Color]time.Duration{
+				piece.White: p.Clocks[piece.White],
+				piece.Black: p.Clocks[piece.Black],
+			},
+		}
+	}
+	return n
 }
--- data/services/tournament/openings_test.go
+++ data/services/tournament/openings_test.go
@@ -1,9 +1,46 @@
 package tournament
 
 import (
+	"fmt"
+	"github.com/andrewbackes/chess/game"
+	"github.com/andrewbackes/tourney/data/models"
 	"testing"
 )
 
-func TestDeepen(t *testing.T) {
+func TestSetOpenings(t *testing.T) {
+	trnmt := newTournament()
+	trnmt.Games = models.NewGameList("tId", trnmt.Settings) // TODO: mock this
+	setGameOpenings(trnmt)
+	fmt.Println(trnmt.Games)
+}
 
+func newTournament() *models.Tournament {
+	return &models.Tournament{
+		Settings: models.Settings{
+			TestSeats: 1,
+			Carousel:  false,
+			Rounds:    10,
+			Engines: []models.Engine{
+				models.Engine{
+					Name: "tester1",
+				},
+				models.Engine{
+					Name: "tester2",
+				},
+			},
+			TimeControl: game.TimeControl{
+				Moves:     40,
+				Time:      1000000000,
+				Repeating: true,
+			},
+			Opening: models.Opening{
+				Depth:     8,
+				Randomize: true,
+				Book: models.Book{
+					FilePath: "/Users/Andrew/tourney_books/2700draw.bin",
+					MaxDepth: 14,
+				},
+			},
+		},
+	}
 }
