8e3e890fdcc0c1a01691fae39b4750836b791ce4
andrewbackes <rbackes@ucla.edu>
2017-11-25 19:13:37 -0800

    Persist engine data

--- data/stores/memdb/engines.go
+++ data/stores/memdb/engines.go
@@ -1,7 +1,13 @@
 package memdb
 
 import (
+	"encoding/json"
 	"github.com/andrewbackes/tourney/data/models"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func (m *MemDB) ReadEngines() []*models.Engine {
@@ -17,60 +23,55 @@ func (m *MemDB) CreateEngine(e *models.Engine) {
 	_, exists := m.engines.Load(e.Id())
 	if !exists {
 		m.engines.Store(e.Id(), e)
+		m.persistEngine(e)
+	}
+}
+
+func (m *MemDB) persistEngine(e *models.Engine) {
+	if !m.persisted() {
+		return
+	}
+	engineDir := filepath.Join(m.backupDir, "engines")
+	err := os.MkdirAll(engineDir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	engineJSON := filepath.Join(engineDir, e.Id()+".json")
+	f, err := os.Create(engineJSON)
+	if err != nil {
+		panic(err)
+	}
+	log.Info("Persisting engine", engineJSON)
+	err = json.NewEncoder(f).Encode(e)
+	if err != nil {
+		panic(err)
 	}
 }
 
 func (m *MemDB) restoreEngines() {
-	/*
-		root := filepath.Join(m.backupDir, "tournaments")
-		if _, err := os.Stat(root); os.IsNotExist(err) {
-			log.Info("No tournaments found in ", root)
-			return
-		}
-		files, err := ioutil.ReadDir(root)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range files {
-			if strings.HasSuffix(file.Name(), ".json") {
-				log.Debug("Restoring tournament ", file.Name())
-				f, err := os.Open(filepath.Join(root, file.Name()))
-				if err != nil {
-					log.Fatal(err)
-				}
-				t := &models.Tournament{}
-				err = json.NewDecoder(f).Decode(t)
-				if err != nil {
-					log.Fatal(err)
-				}
-				m.tournaments.Store(t.Id, t)
-				m.locks.Store(t.Id, &sync.Mutex{})
-				f.Close()
-				gameDir := filepath.Join(root, strings.TrimSuffix(file.Name(), ".json"))
-				gameFiles, err := ioutil.ReadDir(gameDir)
-				if err != nil {
-					log.Fatal(err)
-				}
-				for _, gameFile := range gameFiles {
-					gf, err := os.Open(filepath.Join(gameDir, gameFile.Name()))
-					if err != nil {
-						log.Fatal(err)
-					}
-					g := models.Game{}
-					err = json.NewDecoder(gf).Decode(&g)
-					if err != nil {
-						log.Fatal(err)
-					}
-					m.games.Store(g.Id, &g)
-					m.locks.Store(g.Id, &sync.Mutex{})
-					t, ok := m.tournaments.Load(g.TournamentId)
-					if !ok {
-						log.Fatal("can not add games to a tournament that does not exist")
-					}
-					t.(*models.Tournament).Games = append(t.(*models.Tournament).Games, &g)
-					gf.Close()
-				}
+	root := filepath.Join(m.backupDir, "engines")
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		log.Info("No engines found in ", root)
+		return
+	}
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".json") {
+			log.Debug("Restoring engine ", file.Name())
+			f, err := os.Open(filepath.Join(root, file.Name()))
+			if err != nil {
+				log.Fatal(err)
 			}
+			e := &models.Engine{}
+			err = json.NewDecoder(f).Decode(e)
+			if err != nil {
+				log.Fatal(err)
+			}
+			m.engines.Store(e.Id(), e)
+			f.Close()
 		}
-	*/
+	}
 }
